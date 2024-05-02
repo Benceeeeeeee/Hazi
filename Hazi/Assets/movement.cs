858cd89505acd37da92da3ad35c63d336c3ad1c9
Benceeeeeeee <144451434+Benceeeeeeee@users.noreply.github.com>
2024-05-02 19:43:05 +0200

    script_movement

--- Hazi/Assets/movement.cs
+++ Hazi/Assets/movement.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    float horizontalInput;
+    float moveSpeed = 5f;
+    bool isFacingRight = false;
+    float jumpPower = 5f;
+    bool isGrounded = true;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        FlipSprite();
+
+        if(Input.GetButton("Jump") && !isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isGrounded = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2 (horizontalInput * moveSpeed, rb.velocity.y);
+    }
+
+    void FlipSprite()
+    {
+        if(isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
     }
 }
