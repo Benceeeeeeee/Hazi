eb9ab10af5a5e0cc92d452df72e734b6b63e82d8
Benceeeeeeee <144451434+Benceeeeeeee@users.noreply.github.com>
2024-05-02 20:16:37 +0200

    ugras_javitasa

--- Hazi/Assets/movement.cs
+++ Hazi/Assets/movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,14 +9,16 @@ public class movement : MonoBehaviour
     float moveSpeed = 5f;
     bool isFacingRight = false;
     float jumpPower = 5f;
-    bool isGrounded = true;
+    bool isGrounded = false;
 
     Rigidbody2D rb;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,16 +27,19 @@ public class movement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         FlipSprite();
 
-        if(Input.GetButton("Jump") && !isGrounded)
+        if(Input.GetButton("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
             isGrounded = false;
+            animator.SetBool("isJumping", !isGrounded);
         }
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2 (horizontalInput * moveSpeed, rb.velocity.y);
+        animator.SetFloat("xVelocity", Math.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
     }
 
     void FlipSprite()
@@ -47,8 +53,9 @@ public class movement : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         isGrounded = true;
+        animator.SetBool("isJumping", !isGrounded);
     }
 }
